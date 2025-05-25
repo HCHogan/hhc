abfe23a5a8cc2388cc5ba58d4adf5f69b8b673be
hank <ysh2291939848@outlook.com>
2025-05-25 19:02:55 +0800

    pretty

--- src/HHC/Core/Pretty.hs
+++ src/HHC/Core/Pretty.hs
@@ -1,7 +1,5 @@
 module HHC.Core.Pretty
-  ( pprExpr,
-    pprType,
-    renderExpr,
+  ( renderExpr,
     renderType,
   )
 where
@@ -10,137 +8,137 @@ import HHC.Core.Syntax
 import Text.PrettyPrint
 import Prelude hiding ((<>))
 
--- | 渲染 ExprP 为 Doc
-pprExpr :: ExprP String -> Doc
-pprExpr expr = case expr of
-  Var v ->
-    pprTermVar v
-  Lam v ty body ->
-    -- λ(x:τ). body
-    char 'λ'
-      <> parens (pprTermVar v <> colon <> pprType ty)
-      <> char '.'
-      <> space
-      <> pprExpr body
-  App f x ->
-    let fDoc = if isAtomicExpr f then pprExpr f else parens (pprExpr f)
-        xDoc = if isAtomicExpr x then pprExpr x else parens (pprExpr x)
-     in fDoc <+> xDoc
-  TyLam tv _kind body ->
-    -- Λ a. body
-    char 'Λ' <+> pprTypeVar tv <> char '.' <+> pprExpr body
-  TyApp e ty ->
-    let eDoc = if isAtomicExpr e then pprExpr e else parens (pprExpr e)
-     in eDoc <> char '@' <> pprType ty
-  Let v rhs body ->
-    text "let"
-      <+> pprTermVar v
-      <+> equals
-      <+> pprExpr rhs
-      <+> text " in "
-      <> pprExpr body
-  Con dc tys args ->
-    -- DataCon @t1 @t2 a b
-    let dcDoc = pprDataCon dc
-        tyDocs = map (\t -> char '@' <> pprType t) tys
-        argDocs = map (\a -> if isAtomicExpr a then pprExpr a else parens (pprExpr a)) args
-     in hsep (dcDoc : tyDocs ++ argDocs)
-  Case scrut alts ->
-    text "case" <+> pprExpr scrut <+> text "of"
-      $$ nest 2 (vcat (map pprAlt alts))
-  Lit l ->
-    pprLit l
+maybeParens :: Bool -> Doc -> Doc
+maybeParens True doc = parens doc
+maybeParens False doc = doc
 
--- | 渲染 AltP
-pprAlt :: AltP String -> Doc
-pprAlt (Alt pat rhs) =
-  pprPat pat <+> text "->" <+> pprExpr rhs
+letPrec, lamPrec, casePrec :: Int
+letPrec = 0
+lamPrec = 0
+casePrec = 0
 
--- | 渲染模式
-pprPat :: PatP String -> Doc
-pprPat = \case
-  PVar v -> pprTermVar v
-  PLit l -> pprLit l
-  PCon dc vs ->
-    pprDataCon dc <+> hsep (map pprTermVar vs)
+appPrec :: Int
+appPrec = 10
 
--- | 渲染类型
-pprType :: TypeP String -> Doc
-pprType tp = case tp of
-  TVar v ->
-    pprTypeVar v
-  TCon c ->
-    pprTyCon c
-  TArr t1 t2 ->
-    let t1d = if isArrowType t1 then parens (pprType t1) else pprType t1
-     in t1d <+> text "->" <+> pprType t2
-  TApp t1 t2 ->
-    let d1 = if isAtomicType t1 then pprType t1 else parens (pprType t1)
-        d2 = if isAtomicType t2 then pprType t2 else parens (pprType t2)
-     in d1 <+> d2
-  TForall v _kind t ->
-    -- forall (a :: κ). τ
-    text "forall"
-      <+> parens (pprTypeVar v <+> text "::" <+> pprKind _kind)
-      <> text "."
-      <+> pprType t
+tyAppPrec :: Int
+tyAppPrec = 11
 
--- | 渲染 Kind
-pprKind :: Kind -> Doc
-pprKind = \case
-  KStar -> char '*'
-  KFun a b ->
-    let da = if isKFun a then parens (pprKind a) else pprKind a
-     in da <+> text "->" <+> pprKind b
+atomPrec :: Int
+atomPrec = 12
 
--- | 渲染 Literal
-pprLit :: Literal -> Doc
-pprLit = \case
-  LInt i -> integer i
-  LChar c -> text (show c)
-  LString s -> text (show s)
-  LBool b -> text (if b then "True" else "False")
+forallPrec :: Int
+forallPrec = 0
 
--- | 基本名称打印
-pprTermVar :: TermVarP String -> Doc
-pprTermVar (TermVar s) = text s
+arrPrec :: Int
+arrPrec = 1
 
-pprTypeVar :: TypeVarP String -> Doc
-pprTypeVar (TypeVar s) = text s
+class Pretty a where
+  ppr :: a -> Doc
+  pprPrec :: Int -> a -> Doc
+  pprPrec _ = ppr
 
-pprTyCon :: TyConP String -> Doc
-pprTyCon (TyCon s) = text s
+instance Pretty String where
+  ppr = text
+  pprPrec _ = text
 
-pprDataCon :: DataConP String -> Doc
-pprDataCon (DataCon s) = text s
+instance Pretty (TermVarP String) where
+  ppr (TermVar s) = text s
+  pprPrec _ (TermVar s) = text s
 
--- | 判断是否需要加括号
-isAtomicExpr :: ExprP a -> Bool
-isAtomicExpr = \case
-  Var {} -> True
-  Lit {} -> True
-  Con {} -> True
-  _ -> False
+instance Pretty (TypeVarP String) where
+  ppr (TypeVar s) = text s
+  pprPrec _ (TypeVar s) = text s
 
-isAtomicType :: TypeP a -> Bool
-isAtomicType = \case
-  TVar {} -> True
-  TCon {} -> True
-  _ -> False
+instance Pretty (TyConP String) where
+  ppr (TyCon s) = text s
+  pprPrec _ (TyCon s) = text s
 
-isArrowType :: TypeP a -> Bool
-isArrowType = \case
-  TArr {} -> True
-  _ -> False
+instance Pretty (DataConP String) where
+  ppr (DataCon s) = text s
+  pprPrec _ (DataCon s) = text s
 
-isKFun :: Kind -> Bool
-isKFun = \case
-  KFun {} -> True
-  _ -> False
+instance Pretty Kind where
+  ppr = pprKindPrec 0
+  pprPrec = pprKindPrec
 
--- | 方便渲染为 String
-renderExpr :: ExprP String -> String
-renderExpr = render . pprExpr
+pprKindPrec :: Int -> Kind -> Doc
+pprKindPrec _ KStar = text "*"
+pprKindPrec d (KFun k1 k2) =
+  maybeParens (d > arrPrec) $
+    pprKindPrec (arrPrec + 1) k1 <+> text "→" <+> pprKindPrec arrPrec k2
 
-renderType :: TypeP String -> String
-renderType = render . pprType
+instance Pretty (TypeP String) where
+  ppr = pprTypePrec 0
+  pprPrec = pprTypePrec
+
+pprTypePrec :: Int -> TypeP String -> Doc
+pprTypePrec _ (TVar v) = ppr v
+pprTypePrec _ (TCon tc) = ppr tc
+pprTypePrec d (TArr t1 t2) =
+  maybeParens (d > arrPrec) $
+    pprTypePrec (arrPrec + 1) t1 <+> text "→" <+> pprTypePrec arrPrec t2
+pprTypePrec d (TForall tv k ty) =
+  maybeParens (d > forallPrec) $
+    hang
+      (text "∀(" <> ppr tv <+> text "::" <+> pprKindPrec 0 k <> text ").") -- Changed : to :: and added spaces
+      2
+      (pprTypePrec forallPrec ty)
+pprTypePrec d (TApp t1 t2) =
+  maybeParens (d > appPrec) $
+    pprTypePrec appPrec t1 <+> pprTypePrec (appPrec + 1) t2
+
+instance Pretty Literal where
+  ppr (LInt i) = integer i
+  ppr (LChar c) = quotes (char c)
+  ppr (LString s) = doubleQuotes (text s)
+  ppr (LBool True) = text "True"
+  ppr (LBool False) = text "False"
+
+instance Pretty (PatP String) where
+  ppr (PVar v) = ppr v
+  ppr (PLit l) = ppr l
+  ppr (PCon dc vs) = hsep (ppr dc : map ppr vs)
+
+instance Pretty (AltP String) where
+  ppr (Alt pat expr) =
+    ppr pat <+> text "->" <+> pprPrec 0 expr
+
+instance Pretty (ExprP String) where
+  ppr = pprPrec 0
+  pprPrec = pprExprPrec
+
+pprExprPrec :: Int -> ExprP String -> Doc
+pprExprPrec _ (Var v) = ppr v
+pprExprPrec _ (Lit l) = ppr l
+pprExprPrec d (Lam x ty body) =
+  maybeParens (d > lamPrec) $
+    text "λ(" <> ppr x <+> text "::" <+> pprTypePrec 0 ty <> text ")." <+> pprExprPrec lamPrec body
+pprExprPrec d (App e1 e2) =
+  maybeParens (d > appPrec) $
+    pprExprPrec appPrec e1 <+> pprExprPrec (appPrec + 1) e2
+pprExprPrec d (TyLam tv k body) =
+  maybeParens (d > lamPrec) $
+    text "Λ(" <> ppr tv <+> text "::" <+> pprKindPrec 0 k <> text ")." <+> pprExprPrec lamPrec body
+pprExprPrec d (TyApp e ty) =
+  maybeParens (d > tyAppPrec) $
+    pprExprPrec tyAppPrec e <+> char '@' <> pprTypePrec atomPrec ty
+pprExprPrec d (Let x e1 e2) =
+  maybeParens (d > letPrec) $
+    text "let" <+> ppr x <+> equals <+> pprExprPrec 0 e1 <+> text "in" <+> pprExprPrec letPrec e2
+pprExprPrec _ (Con dc tyArgs exArgs) =
+  let dcDoc = ppr dc
+      tyDocs = map (\ty -> char '@' <> pprTypePrec atomPrec ty) tyArgs
+      exDocs = map (pprExprPrec (appPrec + 1)) exArgs
+   in hsep (dcDoc : tyDocs ++ exDocs)
+pprExprPrec d (Case scrut alts) =
+  maybeParens (d > casePrec) $
+    hang
+      (text "case" <+> pprExprPrec 0 scrut <+> text "of")
+      2
+      (vcat (map ppr alts))
+
+renderExpr :: Expr -> String
+renderExpr expr = render (ppr expr)
+
+renderType :: Type -> String
+renderType ty = render (ppr ty)
