28423afce33034ee975dd4229837cf7f70c400f4
Hank Hogan <ysh2291939848@outlook.com>
2025-05-27 01:22:19 +0800

    update

--- flake.nix
+++ flake.nix
@@ -16,20 +16,59 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        name = "puzzel";
+        name = "hhc";
         src = ./.;
+        llvm16 = pkgs.llvmPackages_16;
       in {
-        packages.default = derivation {
-          inherit system name src;
-          builder = with pkgs; "${bash}/bin/bash";
-          args = ["-c" "echo foo > $out"];
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = name;
+          version = "0.1.0";
+          src = ./.;
+
+          nativeBuildInputs = [
+            pkgs.haskell.compiler.ghc912
+            pkgs.cabal-install
+
+            llvm16.libllvm
+            llvm16.bintools
+            llvm16.libcxxClang
+          ];
+
+          unpackPhase = "true";
+
+          buildPhase = ''
+            mkdir -p /tmp/cabal
+            mkdir -p /tmp/dist-newstyle
+            export CABAL_DIR=/tmp/cabal
+            if [ ! -f "$CABAL_DIR/config" ]; then
+              cabal user-config init
+            fi
+            cabal update
+            cd $src
+            cabal -j build --enable-tests --builddir=/tmp/dist-newstyle
+          '';
+
+          checkPhase = ''
+            export CABAL_DIR=/tmp/cabal
+            cabal test --enable-tests --show-details=always
+          '';
+
+          installPhase = ''
+            export CABAL_DIR=/tmp/cabal
+            cd "$src"
+            cabal install \
+              --builddir=/tmp/dist-newstyle \
+              --install-method=copy \
+              --installdir="$out/bin" \
+              --overwrite-policy=always
+          '';
         };
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             gnumake
-            llvmPackages_16.libllvm
-            llvmPackages_16.bintools
-            llvmPackages_16.libcxxClang
+            llvm16.libllvm
+            llvm16.bintools
+            llvm16.libcxxClang
 
             haskell.compiler.ghc912
             haskell.packages.ghc912.haskell-language-server
